08041eaedf17d5fdfa963b89e872c0c79da522ff
Almas201 <50501939+Almas201@users.noreply.github.com>
2023-03-13 03:46:44 +0600

    Update check_credentials.py

--- check_credentials.py
+++ check_credentials.py
@@ -13,8 +13,8 @@ def check_credentials(username, password):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.binary_location = '/usr/bin/google-chrome'
-    driver = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')
+    options.binary_location = '/usr/bin/'
+    driver = webdriver.Chrome(options=options, executable_path='/usr/bin/')
     
     # Navigate to the website
     driver.get(BASE_URL)
